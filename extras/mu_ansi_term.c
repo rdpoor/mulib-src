7644c9c3d3028c0f16c58c8f549a6e7cceff6d5f
R. D. Poor <rdpoor@gmail.com>
2021-06-24 10:21:27 -0700

    WIP Extending extras/mu_ansi_term to support colors, cursor pos

--- extras/mu_ansi_term.c
+++ extras/mu_ansi_term.c
@@ -38,39 +38,38 @@
 // =============================================================================
 // Local storage
 
+static mu_ansi_term_color_t s_fg_color;
+static mu_ansi_term_color_t s_bg_color;
+
+#undef ANSI_TERM_COLOR
+#define ANSI_TERM_COLOR(MU_ANSI_TERM__name, _fg, _bg) _fg,
+static const uint8_t s_fg_colormap[] = { DEFINE_ANSI_TERM_COLORS };
+
+#undef ANSI_TERM_COLOR
+#define ANSI_TERM_COLOR(MU_ANSI_TERM__name, _fg, _bg) _bg,
+static const uint8_t s_bg_colormap[] = { DEFINE_ANSI_TERM_COLORS };
+
 // =============================================================================
 // Local (forward) declarations
 
+static char getint(uint8_t *val);
+static void set_colors()
+static uint8_t map_fg_color(mu_ansi_term_color_t color);
+static uint8_t map_bg_color(mu_ansi_term_color_t color);
+
 // =============================================================================
 // Public code
+
+void mu_ansi_term_init(void) {
+  mu_ansi_term_set_foreground_color(MU_ANSI_TERM_WHITE);
+  mu_ansi_term_set_background_color(MU_ANSI_TERM_BLACK);
+}
 
 /**
  * @brief Move cursor to 0, 0
  */
 void mu_ansi_term_home(void) {
   puts(CSI "H");
-}
-
-/**
- * @brief Cursor Position
- *
- * Note: assumes row and col are 0 based, but converts to 1 based for ANSI spec.
- */
-void mu_ansi_term_cursor_position(uint8_t row, uint8_t col) {
-  // optimize.
-  if (row == 0) {
-    if (col == 0) {
-      puts(CSI "H");
-    } else {
-      printf(CSI ";%dH", col+1);
-    }
-  } else {
-    if (col == 0) {
-      printf(CSI "%dH", row+1);
-    } else {
-      printf(CSI "%d;%dH", row+1, col+1);
-    }
-  }
 }
 
 /**
@@ -108,5 +107,98 @@ void mu_ansi_term_clear_to_end_of_line(void) {
   puts(CSI "K");
 }
 
+/**
+ * @brief Set Cursor Position
+ *
+ * Note: assumes row and col are 0 based, but converts to 1 based for ANSI spec.
+ */
+void mu_ansi_term_set_cursor_position(uint8_t row, uint8_t col) {
+  // optimize.
+  if (row == 0) {
+    if (col == 0) {
+      puts(CSI "H");
+    } else {
+      printf(CSI ";%dH", col+1);
+    }
+  } else {
+    if (col == 0) {
+      printf(CSI "%dH", row+1);
+    } else {
+      printf(CSI "%d;%dH", row+1, col+1);
+    }
+  }
+}
+
+bool mu_ansi_term_get_cursor_position(uint8_t *row, uint8_t *col) {
+  char ch;
+  uint8_t temp_row;
+  uint8_t temp_col;
+
+  puts(CSI "6n");   // device status reports.  responds with ESC[<row>;<col>R
+  ch = getchar();
+  if (ch != '\e') {
+    ungetc(ch);
+    return false;
+  }
+  ch = getchar();
+  if (ch != '[') {
+    ungetc(ch);
+    return false;
+  }
+  ch = getint(&temp_row);
+  if (ch != ';') {
+    ungetc(ch);
+    return false;
+  }
+  ch = getint(&temp_col);
+  if (ch != 'R') {
+    ungetc(ch);
+    return false;
+  }
+
+  *row = temp_row - 1;
+  *col = temp_col - 1;
+  return true;
+}
+
+/**
+ * @brief Set foreground and background color
+ */
+void mu_ansi_term_set_colors(mu_ansi_term_color_t fg, mu_ansi_term_color_t bg) {
+  s_fg_color = fg;
+  s_bg_color = bg;
+  printf(CSI "%d;%dm", map_fg_color(fg), map_bg_color(bg));
+}
+
+/**
+ * @brief Get foreground and background color
+ */
+void mu_ansi_term_get_colors(mu_ansi_term_color_t *fg, mu_ansi_term_color_t *bg) {
+  *fg = s_fg_color;
+  *bg = s_bg_color;
+}
+
 // =============================================================================
 // Local (static) code
+
+static char getint(uint8_t *val) {
+  char ch;
+  *val = 0;
+  while(1) {
+    ch = getchar();
+    if ((ch >= '0' && (ch <= '9'))) {
+      *val = (*val * 10) + ch - '0';
+    } else {
+      break;
+    }
+  }
+  return ch;  // return char that terminated the run of digits
+}
+
+static uint8_t map_fg_color(mu_ansi_term_color_t color) {
+  return s_fg_colormap[color];
+}
+
+static uint8_t map_bg_color(mu_ansi_term_color_t color) {
+  return s_bg_colormap[color];
+}
--- extras/mu_ansi_term.h
+++ extras/mu_ansi_term.h
@@ -43,18 +43,42 @@ extern "C" {
 // =============================================================================
 // types and definitions
 
+#define DEFINE_ANSI_TERM_COLORS                                                \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_BLACK, 30, 40)                                  \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_RED, 31, 41)                                    \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_GREEN, 32, 42)                                  \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_YELLOW, 33, 43)                                 \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_BLUE, 34, 44)                                   \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_MAGENTA, 35, 45)                                \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_CYAN, 36, 46)                                   \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_WHITE, 37, 47)                                  \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_GRAY, 90, 100)                                  \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_BRIGHT_RED, 91, 101)                            \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_BRIGHT_GREEN, 92, 102)                          \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_BRIGHT_YELLOW, 93, 103)                         \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_BRIGHT_BLUE, 94, 104)                           \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_BRIGHT_MAGENTA, 95, 105)                        \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_BRIGHT_CYAN, 96, 106)                           \
+  ANSI_TERM_COLOR(MU_ANSI_TERM_BRIGHT_WHITE, 97, 107)
+
+#undef ANSI_TERM_COLOR
+#define ANSI_TERM_COLOR(MU_ANSI_TERM__name, _foreground, _background) _name,
+typedef enum {
+  DEFINE_ANSI_TERM_COLORS
+} ansi_term_color_t;
+
 // =============================================================================
 // declarations
+
+/**
+ * @brief Initialize the ansi terminal module.
+ */
+void mu_ansi_term_init(void);
 
 /**
  * @brief Move cursor to 0, 0
  */
 void mu_ansi_term_home(void);
-
-/**
- * @brief Cursor Position
- */
-void mu_ansi_term_cursor_position(uint8_t row, uint8_t col);
 
 /**
  * @brief Erase screen and scrollback
@@ -81,6 +105,27 @@ void mu_ansi_term_clear_line(void);
  */
 void mu_ansi_term_clear_to_end_of_line(void);
 
+/**
+ * @brief Set Cursor Position
+ */
+void mu_ansi_term_set_cursor_position(uint8_t row, uint8_t col);
+
+/**
+ * @brief Get Cursor Position
+ *
+ * Return true if the returned string was the correct format.
+ */
+bool mu_ansi_term_get_cursor_position(uint8_t *row, uint8_t *col);
+
+/**
+ * @brief Set foreground and background color
+ */
+void mu_ansi_term_set_colors(mu_ansi_term_color_t fg, mu_ansi_term_color_t bg);
+
+/**
+ * @brief Get foreground and background color
+ */
+void mu_ansi_term_get_colors(mu_ansi_term_color_t *fg, mu_ansi_term_color_t *bg);
 
 
 #ifdef __cplusplus
